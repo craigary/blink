a8574bc6c8539ee4fb6af6a07b2e2b385682476e
Craig Hart <i@craigary.net>
2018-12-15 17:31:10 +0800

    upade datebase structure and sql queries

--- index.php
+++ index.php
@@ -3,21 +3,27 @@
 	require('includes/header.php'); ?>
 		<div class="title is-parent">
 		<?php
-			$result = mysqli_query($db, 'SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
-
+			$result = mysqli_query($db, 'SELECT * FROM iceland_contents ORDER BY cid DESC');
 			$pages = new Paginator($postsPerPage,'p');
-			$sql = 'SELECT postID FROM blog_posts';
+			$sql = 'SELECT cid FROM iceland_contents';
 			$stmt = mysqli_query($db,$sql);
 			$pages->set_total(mysqli_num_rows($stmt));
-			$sql = 'SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC '.$pages->get_limit();
+			$sql = 'SELECT * FROM iceland_contents ORDER BY cid DESC'.$pages->get_limit();
 			$stmt = mysqli_query($db,$sql);
 			while($row = mysqli_fetch_assoc($stmt)){
 				echo '<div class="post">';
-						echo '<h1 class="post_title"><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-						echo '<div class="post_info"><p class="post_date">Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-						echo '<p class="post_category"><a href="#">#trial</a></p></div>';// insert post category here
-						echo '<p class="desc">'.$row['postDesc'].'</p>';
-						echo '<p><a href="viewpost.php?id='.$row['postID'].'" class="post_readmore">Read More</a></p>';
+						echo '<h1 class="post_title"><a href="viewpost.php?id='.$row['cid'].'">'.$row['title'].'</a></h1>';
+						echo '<div class="post_info"><p class="post_date">Posted on '.date('jS M Y H:i:s', strtotime($row['modified'])).'</p>';
+						echo '<p class="post_category"><a href="#">#trial</a></p></div>';
+
+							$categoryid = $row['category'];
+							$sql = 'SELECT * FROM iceland_categories WHERE cid = '.$categoryid.'ORDER BY mid DESC';
+							$stmt = mysqli_query($db,$sql);
+							$cate = mysqli_fetch_assoc($stmt);
+							echo '<p class="post_category"><a href="#" title="'.$cate['description'].'">#'.$cate['name'].'</a></p></div>';
+						
+						echo '<p class="desc">'.$row['description'].'</p>';
+						echo '<p><a href="viewpost.php?id='.$row['cid'].'" class="post_readmore">Read More</a></p>';
 						echo '<hr>';
 					echo '</div>';
 			}
