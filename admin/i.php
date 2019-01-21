81b914ad0dd6c6e77ff28240df5ef0c064e4f6a2
Craig Hart <i@craigary.net>
2019-01-22 06:16:51 +0800

    yo...

--- admin/i.php
+++ admin/i.php
@@ -1,15 +1,10 @@
 <?php
 require_once('../includes/config.php');
-
-  $sql = "SELECT name,value FROM blink_options WHERE user = 0 or 1";
-  $result = mysqli_query($db, $sql);
-  $jarr = array();
-  while ($rows = mysqli_fetch_assoc($result)) {
-    $jarr[$rows['name']] = $rows['value'];
+  $settingsSql = "SELECT name,value FROM blink_options WHERE user = 0 or 1";
+  $settingsResult = mysqli_query($db, $sql);
+  $settings = array();
+  while ($rows = mysqli_fetch_assoc($settingsResult)) {
+    $settings[$rows['name']] = $rows['value'];
 }
-$arr=json_encode($jarr);
-echo $arr;
-// $settings = json_decode($arr,true);
-// print_r($settings);
-// echo $settings[0][name];
+echo $settings['markdown'];
 ?>
