a9048a44efb940519924d21c543c7ca840838b1c
Craig Hart <i@craigary.net>
2019-04-16 14:21:42 +0800

    test

--- includes/config.php
+++ includes/config.php
@@ -23,6 +23,7 @@ $db = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport);
 if ($db -> connect_error) {
    die("Connection Failed: ".$db->connect_error);
 }
+
 mysqli_set_charset($db,"utf8mb4");
 
 //set timezone
